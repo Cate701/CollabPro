b53fb3ba8b2684ba79c07c0986c525372ba28094
ParkerHitch <parker.hitchcock@gmail.com>
2024-10-26 14:14:33 -0400

    feat: Database initialization

--- src/main/java/org/collabStudios/Main.java
+++ src/main/java/org/collabStudios/Main.java
@@ -1,9 +1,23 @@
 package org.collabStudios;
 
+import org.collabStudios.database.WorkspaceDBClient;
+import org.collabStudios.database.WorkspaceService;
+import org.collabStudios.model.Workspace;
+
+import java.util.List;
+
 public class Main {
 
+    private static void sqlTest() {
+        WorkspaceDBClient client = new WorkspaceDBClient("testWs");
+        System.out.println("Connection");
+        Workspace dummyNew = new Workspace("testWs", List.of(new String[]{"frontend", "backend", "database"}), null, null, 5);
+        WorkspaceService.setupNewWorkspaceBlank(client,dummyNew);
+        client.close();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sqlTest();
     }
 
 }
---
+++ src/main/java/org/collabStudios/database/TaskService.java
@@ -0,0 +1,11 @@
+package org.collabStudios.database;
+
+import org.collabStudios.model.Task;
+
+public class TaskService {
+
+    static void upsertTask(WorkspaceDBClient client, Task task) {
+
+    }
+
+}
