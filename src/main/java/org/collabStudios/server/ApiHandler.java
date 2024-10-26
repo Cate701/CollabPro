0dde3c073bd1cbc48e42c7bbcfeca02759f8194e
jebba <jeb.bazzell@gmail.com>
2024-10-26 17:16:08 -0400

    apihandler and json bullshit

--- src/main/java/org/collabStudios/server/ApiHandler.java
+++ src/main/java/org/collabStudios/server/ApiHandler.java
@@ -3,9 +3,13 @@ package org.collabStudios.server;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.collabStudios.database.WorkspaceDBClient;
+import org.collabStudios.model.Task;
+import org.collabStudios.model.User;
 import org.collabStudios.model.Workspace;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
 
 public class ApiHandler implements HttpHandler {
 
@@ -30,12 +34,30 @@ public class ApiHandler implements HttpHandler {
         }
     }
 
-    private  void getUserList(HttpExchange exchange) {
+    private void getUserList(HttpExchange exchange) throws IOException {
+        List<User> userList = workspace.getUsers();
+
+        // FORMAT: "User [name=" + name + ", id=" + id + ", title=" + title + ", available=" + available + ", currentTasks=" + currentTasks + ", skillLevels" + skillLevels]";
+        String response = workspace.getUsers().toString();
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
 
     }
 
     private void createNewTask(HttpExchange exchange) {
 
+    }
+
+    private void getTaskList(HttpExchange exchange) throws IOException {
+
+        String response = workspace.getTasks().toString();
+        //format: [Task, Task, Task, ...]
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 
 }
